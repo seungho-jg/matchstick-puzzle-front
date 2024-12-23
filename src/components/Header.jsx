09ac6c0abba8faa8dd547489395bf28cc7d97bc7
seungho-jg <tmdgh971229@gmail.com>
2024-12-23 20:09:27 +0900

    Fix: Header z-index 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,7 +10,7 @@ export default function Header() {
   }
 
   return (
-    <header className="bg-white shadow-sm fixed w-full dark:bg-slate-800">
+    <header className="bg-white shadow-sm fixed w-full dark:bg-slate-800 z-50">
       <div className="container mx-auto px-4">
         <div className="flex justify-between h-16 items-center">
           <Link to="/" className="flex items-center">
