da2eb88074a214d62b3073222ea19730873a9388
seungho-jg <tmdgh971229@gmail.com>
2025-01-10 16:47:04 +0900

    Feat: 퍼즐레이아웃 스크롤 방지

--- src/routes/layouts/puzzleLayout.jsx
+++ src/routes/layouts/puzzleLayout.jsx
@@ -1,9 +1,9 @@
-import { Outlet, ScrollRestoration } from "react-router-dom"
+import { Outlet } from "react-router-dom"
 import Header from "../../components/Header"
 
 export default function PuzzleLayout() {
   return(
-    <div className="bg-gray-50 dark:bg-slate-600 min-h-screen overflow-hidden">
+    <div className="fixed inset-0 bg-gray-50 dark:bg-slate-600">
       <Header />
       <main className="mx-auto px-2 py-6 pt-16 h-[calc(100vh-4rem)]">
         <Outlet />
